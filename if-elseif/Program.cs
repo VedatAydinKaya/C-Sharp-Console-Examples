119427555bf9e3ba0fdb5c3b4a32151940e04583
Vedat Kaya <vedataydinkayaa@gmail.com>
2021-10-29 16:57:30 +0300

    if-else-if

---
+++ if-elseif/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            Console.WriteLine(time);
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Gunaydın");
+            else if (time <= 18)
+                Console.WriteLine("İyi gunler");
+            else
+                Console.WriteLine("iyi geceler");
+
+            string sonuc = time <= 18 ? "iyi gunler" : "iyi geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Gunaydın" : time <= 18 ? "İyi gunler" : "iyi geceler";
+
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
